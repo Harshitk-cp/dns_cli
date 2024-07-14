ee0fda1819e2a1ce27ae85a19ce232db4e98a84b
Harshit <harshitk.cp@gmail.com>
2024-07-15 00:10:04 +0530

    support for CNAME record

--- pkg/resolver/query.go
+++ pkg/resolver/query.go
@@ -7,16 +7,43 @@ import (
 )
 
 func queryServers(question dns.Question, servers []string) (*dns.Msg, error) {
+	for _, server := range servers {
+		response, err := queryServer(question, server)
+		if err == nil {
+			for _, answer := range response.Answer {
+				if cname, ok := answer.(*dns.CNAME); ok {
+					cnameQuestion := dns.Question{
+						Name:   cname.Target,
+						Qtype:  question.Qtype,
+						Qclass: dns.ClassINET,
+					}
+					cnameResponse, err := ResolveDNS(cnameQuestion)
+					if err != nil {
+						return nil, err
+					}
+					response.Answer = append(response.Answer, cnameResponse.Answer...)
+				}
+			}
+			return response, nil
+		}
+	}
+	return nil, fmt.Errorf("failed to query any server")
+}
+
+func queryServer(question dns.Question, server string) (*dns.Msg, error) {
 	m := new(dns.Msg)
 	m.SetQuestion(question.Name, question.Qtype)
 	m.RecursionDesired = true
 
-	c := new(dns.Client)
-	for _, server := range servers {
-		response, _, err := c.Exchange(m, server+":53")
-		if err == nil {
-			return response, nil
-		}
+	client := new(dns.Client)
+	in, _, err := client.Exchange(m, server+":53")
+	if err != nil {
+		return nil, err
 	}
-	return nil, fmt.Errorf("failed to query all servers")
+
+	if in.Rcode != dns.RcodeSuccess {
+		return nil, fmt.Errorf("failed to get valid answer")
+	}
+
+	return in, nil
 }
